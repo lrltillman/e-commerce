a71f91b8fee536186ea2919db5bfd8c56bc3a504
Leah Tillman <leahtillman@Leahs-MacBook-Air.local>
2023-11-20 09:07:19 -0600

    first commit containing given code and beginning of routes being completed. some notes taken in category model file.

---
+++ Develop/routes/api/category-routes.js
@@ -0,0 +1,60 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  try {
+    const payload = await Category.findAll({ include: [Product] });
+    res.status(200).json({ status: "success", payload })
+  } catch (err) {
+    res.status(500).json({ status: "error", payload: err.message })
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const payload = await Category.findByPk({ include: [Product] });
+    res.status(200).json({ status: "success", payload })
+  } catch (err) {
+    res.status(500).json({ status: "error", payload: err.message })
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const payload = await Category.create(req.body)
+    res.status(200).json({ status: "success", payload })
+  } catch (err) {
+    res.status(500).json({ status: "error", payload: err.message })
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const payload = await Category.update(
+      req.body,
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+    res.status(200).json({ status: "success", payload })
+  } catch (err) {
+    res.status(500).json({ status: "error", payload: err.message })
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const payload = await Category.destroy({
+      where: [req.params.id]
+    })
+    res.status(200).json({ status: "success", payload })
+  } catch (err) {
+    res.status(500).json({ status: "error", payload: err.message })
+  }
+});
+
+module.exports = router;
---
+++ Develop/routes/api/tag-routes.js
@@ -0,0 +1,60 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  try {
+    const payload = await Tag.findAll({ include: [Product] });
+    res.status(200).json({ status: "success", payload })
+  } catch (err) {
+    res.status(500).json({ status: "error", payload: err.message })
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const payload = await Tag.findByPk({ include: [Product] });
+    res.status(200).json({ status: "success", payload })
+  } catch (err) {
+    res.status(500).json({ status: "error", payload: err.message })
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const payload = await Tag.create(req.body)
+    res.status(200).json({ status: "success", payload })
+  } catch (err) {
+    res.status(500).json({ status: "error", payload: err.message })
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const payload = await Tag.update(
+      req.body,
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+    res.status(200).json({ status: "success", payload })
+  } catch (err) {
+    res.status(500).json({ status: "error", payload: err.message })
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const payload = await Tag.destroy({
+      where: [req.params.id]
+    })
+    res.status(200).json({ status: "success", payload })
+  } catch (err) {
+    res.status(500).json({ status: "error", payload: err.message })
+  }
+});
+
+module.exports = router;
