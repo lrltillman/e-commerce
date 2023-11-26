480c41006000ca3dfba27492ae60015d8fe6e407
Leah Tillman <leahtillman@Leahs-MacBook-Air.local>
2023-11-26 15:26:15 -0600

    added category model, defined columns

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,12 +1,21 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
@@ -18,3 +27,17 @@ Category.init(
 );
 
 module.exports = Category;
+
+// w REACT FE first ok
+// BE 1st has advantages w/ vanilla code
+// MVP: being able to run program before building off
+// server and db(schema) first, models directly after 
+// after models, routes
+// from there, FE html
+// FE js
+// once things are functioning, then do styling 
+
+//show sourcing schema/seeds in terminal
+//running server
+//THEN open insomnia
+//optional: show WB
